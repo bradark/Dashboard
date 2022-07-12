29be0a46eea986077d98401b02b774dbd3c46008
bradley <bradleywilfahrt1@gmail.com>
2022-07-12 00:16:58 -0500

    Setup React App
    Setup react app with a /dashboard

---
+++ client/dash-app-client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function MainNavbar() {
+    return (
+      <div>
+        <h1 className="title">DashApp</h1>
+      </div>
+    );
+  }
+  
+  export default MainNavbar;
---
+++ client/dash-app-client/src/pages/dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as ReactBootStrap from "react-bootstrap";
+import Navbar from "../components/Navbar.jsx";
+
+function Dashboard() {
+    return(
+        <div>
+          <Navbar />
+        </div>
+    );
+}
+
+export default Dashboard;
